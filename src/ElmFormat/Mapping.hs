24b3a6fa0b5c713731f04c3527830666850840f6
Aaron VonderHaar <gruen0aermel@gmail.com>
2019-10-19 14:32:07 -0700

    Use INLINE for convenience MapNamespace and MapReferences instances

--- src/ElmFormat/Mapping.hs
+++ src/ElmFormat/Mapping.hs
@@ -11,25 +11,25 @@ import qualified Reporting.Annotation as A
 class MapNamespace ns1 ns2 a1 a2 where
     mapNamespace :: (ns1 -> ns2) -> a1 -> a2
 
-
 instance MapNamespace a b t1 t2 => MapNamespace a b (A.Annotated ann t1) (A.Annotated ann t2) where
     mapNamespace f (A.A ann t) = A.A ann (mapNamespace f t)
-
+    {-# INLINE mapNamespace #-}
 
 instance MapNamespace a b t1 t2 => MapNamespace a b (Commented t1) (Commented t2) where
     mapNamespace f (Commented pre t post) = Commented pre (mapNamespace f t) post
-
+    {-# INLINE mapNamespace #-}
 
 instance MapNamespace a b t1 t2 => MapNamespace a b (PreCommented t1) (PreCommented t2) where
     mapNamespace f (c, t) = (c, mapNamespace f t)
-
+    {-# INLINE mapNamespace #-}
 
 instance MapNamespace a b t1 t2 => MapNamespace a b (List t1) (List t2) where
     mapNamespace f ts = fmap (mapNamespace f) ts
-
+    {-# INLINE mapNamespace #-}
 
 instance MapNamespace a b (Type' a) (Type' b) where
     mapNamespace = fmap
+    {-# INLINE mapNamespace #-}
 
 
 
@@ -46,36 +46,36 @@ class MapReferences ns1 ns2 a1 a2 where
         -> a1
         -> a2
     mapReferences' (fu, fl) = mapReferences fu fl
-
+    {-# INLINE mapReferences' #-}
 
 instance MapReferences a b t1 t2 => MapReferences a b (A.Annotated ann t1) (A.Annotated ann t2) where
     mapReferences fu fl (A.A ann t) = A.A ann (mapReferences fu fl t)
-
+    {-# INLINE mapReferences #-}
 
 instance MapReferences a b t1 t2 => MapReferences a b (Commented t1) (Commented t2) where
     mapReferences fu fl (Commented pre t post) = Commented pre (mapReferences fu fl t) post
-
+    {-# INLINE mapReferences #-}
 
 instance MapReferences a b t1 t2 => MapReferences a b (PreCommented t1) (PreCommented t2) where
     mapReferences fu fl (c, t) = (c, mapReferences fu fl t)
-
+    {-# INLINE mapReferences #-}
 
 instance MapReferences a b t1 t2 => MapReferences a b (WithEol t1) (WithEol t2) where
     mapReferences fu fl (WithEol t eol) = WithEol (mapReferences fu fl t) eol
-
+    {-# INLINE mapReferences #-}
 
 instance MapReferences a b t1 t2 => MapReferences a b (Pair x t1) (Pair x t2) where
     mapReferences fu fl (Pair k v ml) = Pair k (mapReferences fu fl v) ml
-
+    {-# INLINE mapReferences #-}
 
 instance MapReferences a b t1 t2 => MapReferences a b (List t1) (List t2) where
     mapReferences fu fl ts = fmap (mapReferences fu fl) ts
-
+    {-# INLINE mapReferences #-}
 
 instance MapReferences a b (TypeConstructor a) (TypeConstructor b) where
     mapReferences fu _ (NamedConstructor name) = NamedConstructor (fu name)
     mapReferences _ _ (TupleConstructor n) = TupleConstructor n
-
+    {-# INLINE mapReferences #-}
 
 instance MapReferences a b (Type' a) (Type' b) where
     mapReferences fu fl = \case
